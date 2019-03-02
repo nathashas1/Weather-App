a52bf4dd2ebacd63c8f6394f8364afc3e9ebc555
nathashas1 <nathasha36@gmail.com>
2019-03-02 00:27:17 -0800

    user input

--- src/App.js
+++ src/App.js
@@ -5,7 +5,32 @@ import Weather from './components/weather';
 // import logo from './logo.svg';
 // import './App.css';
 //
+
+
 class App extends React.Component {
+  state = {
+  temperature: undefined,
+  city: undefined,
+  country: undefined,
+  humidity: undefined,
+  description: undefined,
+  error: undefined
+  }
+    weather = async(e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    let proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+    let targetUrl = `https://www.metaweather.com/api/location/search/?query=${city}`
+    // const country = e.target.elements.country.value;
+    const api_call = await fetch(proxyUrl + targetUrl)
+    const response = await api_call.json()
+    const woeid = response[0].woeid
+    let targetUrlWoeid = `https://www.metaweather.com/api/location/${woeid}`
+    const api_call_woeid = await fetch(proxyUrl + targetUrlWoeid)
+    const response_woeid = await api_call_woeid.json()
+    console.log(response_woeid);
+  }
+
   render() {
     return (
       <div>
@@ -18,10 +43,3 @@ class App extends React.Component {
 }
 
 export default App;
-
-
-let weather = async() => {
-  const api_call = await fetch(`https://www.metaweather.com/api/location/search/?query=san`)
-  const response = await api_call.json()
-  console.log(response);
-}
--- src/components/user_input.js
+++ src/components/user_input.js
@@ -1,7 +1,7 @@
 import React from 'react';
-const Form = (props) => {
+const Form = ({weather}) => {
   return (
-      <form onSubmit={props.loadWeather}>
+      <form onSubmit={weather}>
         <input type="text" name="city" placeholder="City..." />
         <input type="text" name="country" placeholder="Country..." />
         <button>Get Weather</button>
