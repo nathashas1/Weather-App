1b75e3f31ab6e338c5b6eca6354318992640f04c
nathashas1 <nathasha36@gmail.com>
2019-03-02 13:05:56 -0800

    non existent city

--- src/App.js
+++ src/App.js
@@ -28,24 +28,30 @@ class App extends React.Component {
       let targetUrl = `https://www.metaweather.com/api/location/search/?query=${city}`
       const api_call = await fetch(proxyUrl + targetUrl)
       const response = await api_call.json()
-      const woeid = response[0].woeid
-      let targetUrlWoeid = `https://www.metaweather.com/api/location/${woeid}`
-      const api_call_woeid = await fetch(proxyUrl + targetUrlWoeid)
-      const response_woeid = await api_call_woeid.json()
-      const weather_details = response_woeid.consolidated_weather[0]
-      this.setState({
-        city: response_woeid.title,
-        max_temp: weather_details.max_temp,
-        min_temp: weather_details.min_temp,
-        humidity: weather_details.humidity,
-        weather_state_abbr: weather_details.weather_state_abbr,
-        wind_speed: weather_details.wind_speed,
-        predictability: weather_details.predictability,
-        error: undefined
-      })
+      if (response.length > 0){
+        const woeid = response[0].woeid
+        let targetUrlWoeid = `https://www.metaweather.com/api/location/${woeid}`
+        const api_call_woeid = await fetch(proxyUrl + targetUrlWoeid)
+        const response_woeid = await api_call_woeid.json()
+        const weather_details = response_woeid.consolidated_weather[0]
+        this.setState({
+          city: response_woeid.title,
+          max_temp: weather_details.max_temp,
+          min_temp: weather_details.min_temp,
+          humidity: weather_details.humidity,
+          weather_state_abbr: weather_details.weather_state_abbr,
+          wind_speed: weather_details.wind_speed,
+          predictability: weather_details.predictability,
+          error: undefined
+        })
+      } else { 
+        this.setState({error: "City does not exist"})
+      }
+
     } else {
     this.setState({error: "Please enter City"})
     }
+
   }
 
   render() {
