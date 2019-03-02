1c3765c98d7fe5493f4101777443160144ab4830
nathashas1 <nathasha36@gmail.com>
2019-03-02 01:12:54 -0800

    get data

--- src/App.js
+++ src/App.js
@@ -8,27 +8,43 @@ import Weather from './components/weather';
 
 
 class App extends React.Component {
-  state = {
-  temperature: undefined,
-  city: undefined,
-  country: undefined,
-  humidity: undefined,
-  description: undefined,
-  error: undefined
-  }
+    state = {
+      city: undefined,
+      max_temp: undefined,
+      min_temp: undefined,
+      humidity: undefined,
+      weather_state_abbr: undefined,
+      wind_speed: undefined,
+      predictability: undefined,
+      error: undefined
+    }
     weather = async(e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
     let proxyUrl = 'https://cors-anywhere.herokuapp.com/'
     let targetUrl = `https://www.metaweather.com/api/location/search/?query=${city}`
-    // const country = e.target.elements.country.value;
     const api_call = await fetch(proxyUrl + targetUrl)
     const response = await api_call.json()
     const woeid = response[0].woeid
     let targetUrlWoeid = `https://www.metaweather.com/api/location/${woeid}`
     const api_call_woeid = await fetch(proxyUrl + targetUrlWoeid)
     const response_woeid = await api_call_woeid.json()
+    const weather_details = response_woeid.consolidated_weather[0]
     console.log(response_woeid);
+    if (city){
+    this.setState({
+      city: response_woeid.title,
+      max_temp: weather_details.max_temp,
+      min_temp: weather_details.min_temp,
+      humidity: weather_details.humidity,
+      weather_state_abbr: weather_details.weather_state_abbr,
+      wind_speed: weather_details.wind_speed,
+      predictability: weather_details.predictability,
+      error: ""
+    })
+  } else {
+    this.setState({error: "Please enter City"})
+  }
   }
 
   render() {
@@ -36,7 +52,16 @@ class App extends React.Component {
       <div>
         <Titles />
         <UserInput weather={this.weather} />
-        <Weather />
+        <Weather city={this.state.city}
+                 maxtemp={this.state.max_temp}
+                 mintemp={this.state.min_temp}
+                 humidity={this.state.humidity}
+                 weatherstateabbr={this.state.weather_state_abbr}
+                 windspeed={this.state.wind_speed}
+                 predictability={this.state.predictability}
+                 error={this.state.error}
+
+          />
       </div>
     );
   }
--- src/components/weather.js
+++ src/components/weather.js
@@ -1,9 +1,21 @@
 import React from 'react';
 const Weather = (props) => {
+  if (props.error) {
+    return(
+      <div>Error: {props.error}</div>
+    )
+  } else {
   return(
        <div>
-      //weather conditions goes here
+          {props.city && <p>City: {props.city}</p>}
+          {props.maxtemp && <p>Maximum Temperature: {props.maxtemp}</p>}
+          {props.mintemp && <p>Minimum Temperature: {props.mintemp}</p>}
+          {props.humidity && <p>Humidity: {props.humidity}</p>}
+          {props.weatherstateabbr && <p>Weather State: {props.weatherstateabbr}</p>}
+          {props.windspeed && <p>Wind Speed: {props.windspeed}</p>}
+          {props.predictability && <p>Predictability: {props.predictability}</p>}
+
        </div>
-   )
+   )}
 }
 export default Weather;
