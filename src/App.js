0b9bedc3436bca759e7b9402e435b84604e2f5e7
nathashas1 <nathasha36@gmail.com>
2019-03-01 23:12:43 -0800

    initial set up

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
+import React from 'react';
+import Titles from './components/title';
+import UserInput from './components/user_input';
+import Weather from './components/weather';
+// import logo from './logo.svg';
+// import './App.css';
+//
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Titles />
+        <UserInput weather={this.weather} />
+        <Weather />
       </div>
     );
   }
 }
 
 export default App;
+
+
+let weather = async() => {
+  const api_call = await fetch(`https://www.metaweather.com/api/location/search/?query=san`)
+  const response = await api_call.json()
+  console.log(response);
+}
---
+++ src/components/title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Titles = (props) => {
+  return (
+    <div>
+      <h1> Weather ForeCast </h1>
+        <p> Enter the city and find the weather... </p>
+    </div>
+  )
+}
+
+export default Titles;
