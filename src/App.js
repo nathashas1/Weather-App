324e56719042fb22e2a1fd715275c0a3ba2f46bf
nathashas1 <nathasha36@gmail.com>
2019-03-05 17:02:35 -0800

    style

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Titles from './components/title';
 import UserInput from './components/user_input';
 import Weather from './components/weather';
-// import logo from './logo.svg';
-import './style/App.css';
-//
+import logo from './logo.svg';
+import './App.css';
+
 
 
 class App extends React.Component {
@@ -21,12 +21,6 @@ class App extends React.Component {
       result: false,
       error: undefined,
       responseWoeid: undefined
-      // id: undefined,
-      // oneDay:undefined,
-      // twoDay:undefined,
-      // threeDay:undefined,
-      // fourDay:undefined,
-      // fiveDay:undefined,
     }
 
     anotherCity = async(e) => {
@@ -45,111 +39,90 @@ class App extends React.Component {
 
 
     weather = id => async(e) => {
-    e.preventDefault();
-    console.log("i",id)
-    let city;
-    if (id === 0){
-      city =   e.target.elements.city.value || this.state.city
-    } else {
-      city = this.state.city;
-    }
-    if (city){
-      let proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-      let targetUrl = `https://www.metaweather.com/api/location/search/?query=${city}`
-      const api_call = await fetch(proxyUrl + targetUrl)
-      const response = await api_call.json()
-      if (response.length > 0){
-        const woeid = response[0].woeid
-        let targetUrlWoeid = `https://www.metaweather.com/api/location/${woeid}`
-        const api_call_woeid = await fetch(proxyUrl + targetUrlWoeid)
-        const response_woeid = await api_call_woeid.json()
-        console.log("res",response_woeid)
-        const weather_details = response_woeid.consolidated_weather[id]
-        const temp = Math.round(weather_details.the_temp)
-        const minTemp = Math.round(weather_details.min_temp)
-        const maxTemp = Math.round(weather_details.max_temp)
-        const windSpeed = Math.round(weather_details.wind_speed)
-        this.setState({
-          city: response_woeid.title,
-          responseWoeid: response_woeid.consolidated_weather,
-          date: weather_details.applicable_date,
-          maxTemp: maxTemp,
-          temp: temp,
-          minTemp: minTemp,
-          humidity: weather_details.humidity,
-          weatherStateAbbr: weather_details.weather_state_abbr,
-          windSpeed: windSpeed,
-          predictability: weather_details.predictability,
-          showComponent: false,
-          result: true,
-          id: weather_details.id,
-          error: undefined,
-        })
-      } else {
-        this.setState({error: "City does not exist"})
+      e.preventDefault();
+      console.log("i",id)
+      let city;
+      if (id === 0){
+         city =   e.target.elements.city.value || this.state.city
+      }else{
+        city = this.state.city;
       }
 
-    } else {
-    this.setState({error: "Please enter City"})
+      if (city){
+        let proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+        let targetUrl = `https://www.metaweather.com/api/location/search/?query=${city}`
+        const api_call = await fetch(proxyUrl + targetUrl)
+        const response = await api_call.json()
+        if (response.length > 0){
+              const woeid = response[0].woeid
+              let targetUrlWoeid = `https://www.metaweather.com/api/location/${woeid}`
+              const api_call_woeid = await fetch(proxyUrl + targetUrlWoeid)
+              const response_woeid = await api_call_woeid.json()
+              console.log("res",response_woeid)
+              const weather_details = response_woeid.consolidated_weather[id]
+              const temp = Math.round(weather_details.the_temp)
+              const minTemp = Math.round(weather_details.min_temp)
+              const maxTemp = Math.round(weather_details.max_temp)
+              const windSpeed = Math.round(weather_details.wind_speed)
+              this.setState({
+                city: response_woeid.title,
+                responseWoeid: response_woeid.consolidated_weather,
+                date: weather_details.applicable_date,
+                maxTemp: maxTemp,
+                temp: temp,
+                minTemp: minTemp,
+                humidity: weather_details.humidity,
+                weatherStateAbbr: weather_details.weather_state_abbr,
+                windSpeed: windSpeed,
+                predictability: weather_details.predictability,
+                showComponent: false,
+                result: true,
+                id: weather_details.id,
+                error: undefined,
+            })
+          }else{
+            this.setState({error: "City does not exist"})
+          }
+      } else {
+        this.setState({error: "Please enter City"})
+      }
     }
 
-  }
-  //
-  // nextDay = async(e,i) => {
-  //   const weather_details = this.state.response_woeid.consolidated_weather[i]
-  //   const temp = Math.round(weather_details.the_temp)
-  //   const minTemp = Math.round(weather_details.min_temp)
-  //   const maxTemp = Math.round(weather_details.max_temp)
-  //   const windSpeed = Math.round(weather_details.wind_speed)
-  //   e.preventDefault();
-  //   this.setState({city: this.state.response_woeid.title,
-  //   maxTemp: maxTemp,
-  //   temp: temp,
-  //   minTemp: minTemp,
-  //   humidity: weather_details.humidity,
-  //   weatherStateAbbr: weather_details.weather_state_abbr,
-  //   windSpeed: windSpeed,
-  //   predictability: weather_details.predictability,
-  //   showComponent: false,
-  //   result: true,
-  //   id: weather_details.id,
-  //   error: undefined, })
-  // }
 
-  render() {
-    let className = 'background';
-    if (this.state.result) {
-      className ='background-result';
+    render() {
+      let className = 'background';
+      if (this.state.result) {
+        className ='background-result';
+      }
+      return (
+          <div className={className}>
+             { this.state.showComponent ? <Titles /> : null }
+             { this.state.showComponent ? <UserInput weather={this.weather} /> : null }
+             { this.state.result?    <Weather city={this.state.city}
+                                      date={this.state.date}
+                                      temp={this.state.temp}
+                                      maxTemp={this.state.maxTemp}
+                                      minTemp={this.state.minTemp}
+                                      humidity={this.state.humidity}
+                                      weatherStateAbbr={this.state.weatherStateAbbr}
+                                      windSpeed={this.state.windSpeed}
+                                      predictability={this.state.predictability}
+                                      error={this.state.error}
+                                      anotherCity={this.anotherCity}
+                                      id={this.state.id}
+                                      weather={this.weather}
+                                      responseWoeid={this.state.responseWoeid}
+                                      oneDay={this.findDay(this.state.responseWoeid[1].applicable_date)}
+                                      twoDay={this.findDay(this.state.responseWoeid[2].applicable_date)}
+                                      threeDay={this.findDay(this.state.responseWoeid[3].applicable_date)}
+                                      fourDay={this.findDay(this.state.responseWoeid[4].applicable_date)}
+                                      fiveDay={this.findDay(this.state.responseWoeid[5].applicable_date)}
+                                      /> : null }
+             { this.state.error? <div className="error">{this.state.error}</div> : null }
+
+          </div>
+      );
     }
-    return (
-        <div className={className}>
-           { this.state.showComponent ? <Titles /> : null }
-           { this.state.showComponent ? <UserInput weather={this.weather} /> : null }
-           { this.state.result?    <Weather city={this.state.city}
-                                    date={this.state.date}
-                                    temp={this.state.temp}
-                                    maxTemp={this.state.maxTemp}
-                                    minTemp={this.state.minTemp}
-                                    humidity={this.state.humidity}
-                                    weatherStateAbbr={this.state.weatherStateAbbr}
-                                    windSpeed={this.state.windSpeed}
-                                    predictability={this.state.predictability}
-                                    error={this.state.error}
-                                    anotherCity={this.anotherCity}
-                                    id={this.state.id}
-                                    weather={this.weather}
-                                    responseWoeid={this.state.responseWoeid}
-                                    oneDay={this.findDay(this.state.responseWoeid[1].applicable_date)}
-                                    twoDay={this.findDay(this.state.responseWoeid[2].applicable_date)}
-                                    threeDay={this.findDay(this.state.responseWoeid[3].applicable_date)}
-                                    fourDay={this.findDay(this.state.responseWoeid[4].applicable_date)}
-                                    fiveDay={this.findDay(this.state.responseWoeid[5].applicable_date)}
-                                    /> : null }
-           { this.state.error? <div className="error">{this.state.error}</div> : null }
-
-        </div>
-    );
-  }
 }
 
 export default App;
--- src/components/weather.js
+++ src/components/weather.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 
 const Weather = (props) => {
-  let image = `weather-${props.weatherStateAbbr}`;
   const hc = <i className="fas fa-cloud" />
-  const gg = <i className="fas fa-cloud" />
   const lc = <i className="fas fa-cloud" />
   const h = <i className="fas fa-cloud-meatball" />
   const s = <i className="fas fa-cloud-showers-heavy" />
@@ -12,42 +10,37 @@ const Weather = (props) => {
   const t = <i className="fas fa-bolt" />
   const c = <i className="far fa-sun" />
   const sn = <i className="far fa-snowflake" />
-  // const hr = "fas fa-cloud-showers-heavy"
   const hr = <i className="fas fa-cloud-showers-heavy" />
   const lr = <i className="fas fa-cloud-rain" />
-  const normalIcon = <i className={eval(props.weatherStateAbbr)} />
-
   return(
     <div className="result-container">
-      <div className="oneSide">
-        <h1>Weekly Forecast</h1>
-        <table style={{backgroundColor: '#FFFFFF', color: '	#000000'}}>
-          <tr><td><button className="btn" onClick={props.weather(0)}>Today</button></td><td><span className="btn">{ eval(props.responseWoeid[0].weather_state_abbr) }</span></td></tr>
-          <tr><td><button className="btn" onClick={props.weather(1)}>{props.oneDay}</button></td><td><span className="btn">{ eval(props.responseWoeid[1].weather_state_abbr) }</span></td></tr>
-          <tr><td><button className="btn" onClick={props.weather(2)}>{props.twoDay}</button></td><td><span className="btn">{ eval(props.responseWoeid[2].weather_state_abbr) }</span></td></tr>
-          <tr><td><button className="btn" onClick={props.weather(3)}>{props.threeDay}</button></td><td><span className="btn">{ eval(props.responseWoeid[3].weather_state_abbr) }</span></td></tr>
-          <tr><td><button className="btn" onClick={props.weather(4)}>{props.fourDay}</button></td><td><span className="btn">{ eval(props.responseWoeid[4].weather_state_abbr) }</span></td></tr>
-          <tr><td><button className="btn" onClick={props.weather(5)}>{props.fiveDay}</button></td><td><span className="btn">{ eval(props.responseWoeid[5].weather_state_abbr) }</span></td></tr>
-        </table>
-      </div>
-
-
-      <div className="oneSide">
-          {props.city && <div className="header"><strong style={{ paddingRight: '20px',fontSize: '35px', color: '#FFFFFF'}}>{props.city}</strong> <strong style={{paddingRight: '20px',fontSize: '35px',color: '#FFFFFF'}}>{props.temp}ºC</strong><span className="btn" style={{paddingTop:'10px'}}>{ eval(props.weatherStateAbbr) }</span></div>}
-          <div className="weather">
-           <table>
-              {props.city && <tbody><tr><th>City</th><td>{props.city}</td></tr></tbody>}
-              {props.date && <tbody><tr><th>Day</th><td>{props.date}</td></tr></tbody>}
-              {props.maxTemp && <tbody><tr><th>Maximum Temperature</th><td>{props.maxTemp}º</td></tr></tbody>}
-              {props.minTemp && <tbody><tr><th>Minimum Temperature</th><td>{props.minTemp}º</td></tr></tbody>}
-              {props.humidity && <tbody><tr><th>Humidity</th><td>{props.humidity}%</td></tr></tbody>}
-              {props.weatherStateAbbr && <tbody><tr><th>Weather State</th><td>{props.weatherStateAbbr}</td></tr></tbody>}
-              {props.windSpeed && <tbody><tr><th>Wind Speed</th><td>{props.windSpeed}mph</td></tr></tbody>}
-              {props.predictability && <tbody><tr><th>Predictability</th><td>{props.predictability}%</td></tr></tbody>}
-           </table>
-         </div>
-         {props.city && <button className="submit2" onClick={props.anotherCity}>Find another City</button>}
+        <div className="weather">
+            <h1 style={{fontSize: '35px', color: '#FFFFFF'}}>Weekly Forecast</h1>
+            <table style={{backgroundColor: '#FFFFFF', color: '	#000000'}}>
+                <tbody><tr><td><button className="btn" onClick={props.weather(0)}>Today</button></td><td><span className="btn">{ eval(props.responseWoeid[0].weather_state_abbr) }</span></td></tr></tbody>
+                <tbody><tr><td><button className="btn" onClick={props.weather(1)}>{props.oneDay}</button></td><td><span className="btn">{ eval(props.responseWoeid[1].weather_state_abbr) }</span></td></tr></tbody>
+                <tbody><tr><td><button className="btn" onClick={props.weather(2)}>{props.twoDay}</button></td><td><span className="btn">{ eval(props.responseWoeid[2].weather_state_abbr) }</span></td></tr></tbody>
+                <tbody><tr><td><button className="btn" onClick={props.weather(3)}>{props.threeDay}</button></td><td><span className="btn">{ eval(props.responseWoeid[3].weather_state_abbr) }</span></td></tr></tbody>
+                <tbody><tr><td><button className="btn" onClick={props.weather(4)}>{props.fourDay}</button></td><td><span className="btn">{ eval(props.responseWoeid[4].weather_state_abbr) }</span></td></tr></tbody>
+                <tbody><tr><td><button className="btn" onClick={props.weather(5)}>{props.fiveDay}</button></td><td><span className="btn">{ eval(props.responseWoeid[5].weather_state_abbr) }</span></td></tr></tbody>
+            </table>
         </div>
+
+
+        <div className="weather">
+            {props.city && <h1 className="header"><strong style={{ paddingRight: '20px',fontSize: '35px', color: '#FFFFFF'}}>{props.city}</strong> <strong style={{paddingRight: '20px',fontSize: '35px',color: '#FFFFFF'}}>{props.temp}ºC</strong><span className="btn" style={{paddingTop:'10px'}}>{ eval(props.weatherStateAbbr) }</span></h1>}
+             <table style={{backgroundColor: '#FFFFFF', color: '	#000000'}}>
+                {props.city && <tbody><tr><th>City</th><td>{props.city}</td></tr></tbody>}
+                {props.date && <tbody><tr><th>Day</th><td>{props.date}</td></tr></tbody>}
+                {props.maxTemp && <tbody><tr><th>Maximum Temperature</th><td>{props.maxTemp}º</td></tr></tbody>}
+                {props.minTemp && <tbody><tr><th>Minimum Temperature</th><td>{props.minTemp}º</td></tr></tbody>}
+                {props.humidity && <tbody><tr><th>Humidity</th><td>{props.humidity}%</td></tr></tbody>}
+                {props.weatherStateAbbr && <tbody><tr><th>Weather State</th><td>{props.weatherStateAbbr}</td></tr></tbody>}
+                {props.windSpeed && <tbody><tr><th>Wind Speed</th><td>{props.windSpeed}mph</td></tr></tbody>}
+                {props.predictability && <tbody><tr><th>Predictability</th><td>{props.predictability}%</td></tr></tbody>}
+             </table>
+           {props.city && <button className="submit2" onClick={props.anotherCity}>Find another City</button>}
+          </div>
     </div>
    )
 }
